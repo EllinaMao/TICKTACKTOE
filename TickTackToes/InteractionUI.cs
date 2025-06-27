cddb1e72f22c6a9b501d24ae1d7059f3c56a5a25
Olga <xotaoshi@gmail.com>
2025-06-27 12:52:50 +0300

    added metods

---
+++ TickTackToes/InteractionUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTackToes
+{
+    internal class InteractionUI
+    {
+        public static void ShowPlayerTurn(bool isX)
+        {
+            MessageBox.Show($"Ход игрока {(isX ? "X" : "O")}", "Ход", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static void ShowWin(bool isX)
+        {
+            MessageBox.Show($"{(isX ? "X" : "O")} победил!", "Победа", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static void ShowDraw()
+        {
+            MessageBox.Show("Ничья!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
